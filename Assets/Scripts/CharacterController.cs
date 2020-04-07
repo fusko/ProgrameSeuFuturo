c9e0804744e660efbd88a50228790a179228ebf5
fusko <diogo2010rs@hotmail.com>
2020-04-07 15:21:17 -0300

    new assests

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -11,34 +11,71 @@ public class CharacterController : MonoBehaviour
     [SerializeField]
     private float maxSpeed;
     private Vector2 direction;
+    [SerializeField]
+    private Animator m_animator;
+    [SerializeField]
+    private bool walking;
+    private bool facingRight;
+    [SerializeField]
+    private float gravity;
 
     void Start()
     {
-
+        Input.simulateMouseWithTouches = true;
         m_rigidbody = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
 
     }
 
     void Update()
     {
+        
+            print("yes");
+            float horizontal = Input.GetAxis("Horizontal");
 
-        if (forwardSpeed < maxSpeed)
-            forwardSpeed += 0.1f * Time.deltaTime;
+        walking = Mathf.Abs(horizontal) != 0 ? true : false;
 
-        float horizontal = Input.GetAxis("Horizontal");
+            direction.Set(horizontal, gravity);
 
-        direction.Set(horizontal, 0);
+            direction.Normalize();
 
-        direction.Normalize(); ;
-
-
-
-
+            Flip(horizontal);
+        
     }
 
     private void FixedUpdate()
     {
-        print("andando");
-        m_rigidbody.MovePosition(m_rigidbody.position + direction * forwardSpeed * Time.deltaTime);
+
+        m_animator.SetBool("walk", walking);
+        m_rigidbody.velocity=(direction * forwardSpeed * Time.deltaTime);
+
     }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal < 0 && !facingRight || horizontal >0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+
+
+    private float HandleInput()
+    {
+        if (Input.touchCount > 0)
+        {
+            float touch;
+            touch = Input.GetTouch(0).position.x;
+            return touch > Screen.width / 2 ? 1 : -1;
+    }
+    else
+    {
+
+            return Input.GetAxis("Horizontal");
+    }
+    }
+
 }
