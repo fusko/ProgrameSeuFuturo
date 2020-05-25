9e845f6c4020d7c26ee162975141a62e43884b76
fusko <diogo2010rs@hotmail.com>
2020-05-25 16:59:13 -0300

    flip do personagem

--- Assets/Scripts/ControlePersonagem.cs
+++ Assets/Scripts/ControlePersonagem.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class CharacterController : MonoBehaviour
+public class ControlePersonagem : MonoBehaviour
 {
     public float velocidade;              
     public Vector3 movimentoHorizontal;
@@ -11,11 +11,13 @@ public class CharacterController : MonoBehaviour
 
     public float gravity;
     public float forcaPulo;
+    public SpriteRenderer spriteRenderer;
 
     void Start()
     {
 
         rb2d = GetComponent<Rigidbody2D>();
+        spriteRenderer= GetComponent<SpriteRenderer>();
     }
 
     void Update() {
@@ -27,8 +29,15 @@ public class CharacterController : MonoBehaviour
      public void Movimentar()
     {
        
+       
         movimentoHorizontal = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
 
+        if  (movimentoHorizontal.x > 0.01f){
+            spriteRenderer.flipX = false;
+        }
+        else if (movimentoHorizontal.x < -0.01f){
+            spriteRenderer.flipX = true;
+        }
         transform.position += movimentoHorizontal * Time.deltaTime * velocidade;
         
     }
@@ -38,5 +47,6 @@ public class CharacterController : MonoBehaviour
             rb2d.AddForce(new Vector2(0f, forcaPulo), ForceMode2D.Impulse);
         }
     }
+
 
 }
