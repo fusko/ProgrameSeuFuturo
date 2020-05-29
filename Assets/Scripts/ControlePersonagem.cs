316da9b102a38b0ddcfd46b65cc84eac447ff5de
fusko <diogo2010rs@hotmail.com>
2020-05-29 20:24:13 -0300

    ajustes nas animações

--- Assets/Scripts/ControlePersonagem.cs
+++ Assets/Scripts/ControlePersonagem.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 
 public class ControlePersonagem : MonoBehaviour
 {
-    public float velocidade;              
+    public float velocidade; 
+     public float velocidadeMax;              
     public Vector3 movimentoHorizontal;
     private Rigidbody2D rb2d;        
 
     public float gravity;
     public float forcaPulo;
     public SpriteRenderer spriteRenderer;
+
+    public Animator animator;
+    public bool pulando;
+    private float aceletacao;
 
     void Start()
     {
@@ -23,6 +28,8 @@ public class ControlePersonagem : MonoBehaviour
     void Update() {
         Movimentar();
         Pular();
+         Animacao();
+    print(-velocidadeMax);
     }
 
     
@@ -31,22 +38,45 @@ public class ControlePersonagem : MonoBehaviour
        
        
         movimentoHorizontal = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        aceletacao =Mathf.Lerp(aceletacao, velocidadeMax* movimentoHorizontal.x, 0.1f);
+       Rotacionar();
+        transform.position += Vector3.right * Time.deltaTime * aceletacao;
+        
+    }
 
-        if  (movimentoHorizontal.x > 0.01f){
+    public void Pular(){
+        if(Input.GetButtonDown("Jump") & pulando == false){//Nota:pode se subistituir "pulando == false" por "!pulando"
+            rb2d.AddForce(new Vector2(0f, forcaPulo), ForceMode2D.Impulse);
+        }
+    }
+
+    public void Rotacionar(){
+         if  (movimentoHorizontal.x > 0.01f){
             spriteRenderer.flipX = false;
         }
         else if (movimentoHorizontal.x < -0.01f){
             spriteRenderer.flipX = true;
         }
-        transform.position += movimentoHorizontal * Time.deltaTime * velocidade;
-        
     }
-
-    public void Pular(){
-        if(Input.GetButtonDown("Jump")){
-            rb2d.AddForce(new Vector2(0f, forcaPulo), ForceMode2D.Impulse);
+    
+    public void Animacao(){
+        animator.SetFloat("velocidadeX",Mathf.Abs(movimentoHorizontal.x));
+         animator.SetFloat("velocidadeY", rb2d.velocity.y);
+   
         }
+      
+    private void OnCollisionEnter2D(Collision2D other) {
+       if(other.gameObject.layer == 8){
+           pulando = false;
+           animator.SetBool("chao",true);
+       } 
     }
 
+    private void OnCollisionExit2D(Collision2D other) {
+            if(other.gameObject.layer == 8){
+           pulando = true;
+            animator.SetBool("chao",false);
+       } 
+    }
+    }
 
-}
